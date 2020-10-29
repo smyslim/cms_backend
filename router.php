1d3972e90510d0d61d44668f0c742e8137a8fd29
Муслим Агакеримов <smyslim@gmail.com>
2020-10-29 13:54:25 +0300

    Update router.php
    added functionality:
    getPages
    editPage
    updatePage

--- router.php
+++ router.php
@@ -6,7 +6,15 @@
   require_once('php/classes/Page.php');
   
   if ($uri[1] == "addPage"){
-    exit(Page::addPage($_POST['html'],$_POST['css'],$_POST['js']));
+    exit(Page::addPage($_POST['html'],$_POST['css'],$_POST['js'],$_POST['name'],$_POST['title']));
   }
-
+  if ($uri[1] == "getPages"){
+    exit(Page::getPages());
+  }
+  if ($uri[1] == "editPage"){
+    exit(Page::editPage($_POST['pageId']));
+  }
+  if ($uri[1] == "updatePage"){
+    exit(Page::updatePage($_POST['pageId'],$_POST['name'],$_POST['title'],$_POST['html'],$_POST['css'],$_POST['js']));
+  }
 ?>
